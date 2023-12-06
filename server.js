42fc4202e41077ff54d72a8e7e47e22cba791be4
vperizes <vperizes@gmail.com>
2023-12-06 12:08:37 -0600

    add express validator middleware to test route

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import express from "express";
 import morgan from "morgan";
 import mongoose from "mongoose";
 import * as dotenv from "dotenv";
+import { body, validationResult } from "express-validator";
 dotenv.config();
 const app = express();
 const port = process.env.PORT;
@@ -38,10 +39,31 @@ app.get("/", (req, res) => {
   res.send("hello there");
 });
 
-app.post("/", (req, res) => {
-  console.log(req);
-  res.json({ msg: "data recieved", data: req.body });
-});
+//adding express middleware for validation layer
+app.post(
+  "/api/v1/test",
+  //checks that name property in body is not empty. if it is it send backt the chained message
+  [
+    body("name")
+      .notEmpty()
+      .withMessage("name is required")
+      .isLength({ min: 2 })
+      .withMessage("name must have at least 2 characters"),
+  ],
+  (req, res, next) => {
+    const errors = validationResult(req);
+    //if isEmpty is false then we have an error and need to return msg
+    if (!errors.isEmpty()) {
+      const errMessages = errors.array().map((error) => error.msg); //getting msg property from error object
+      return res.status(404).json({ errors: errMessages });
+    }
+    next();
+  },
+  (req, res) => {
+    const { name } = req.body;
+    res.json({ msg: `hello ${name}` });
+  }
+);
 
 ///creating starting url for controllers/routes
 app.use("/api/v1/jobs", jobRouter);
