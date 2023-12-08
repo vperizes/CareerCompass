31787a0099f5282c909ab2964add67280c7b7566
vperizes <vperizes@gmail.com>
2023-12-07 22:52:00 -0600

    add create new user logic to register controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,9 @@
+import { StatusCodes } from "http-status-codes";
 import UserModel from "../models/UserModel.js";
 
 export const register = async (req, res) => {
-  res.send("register");
+  const newUser = await UserModel.create(req.body);
+  res.status(StatusCodes.CREATED).json({ newUser });
 };
 
 export const login = async (req, res) => {
