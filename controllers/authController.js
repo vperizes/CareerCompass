1b44f5ddff94fc5c830fd249d898a5dc8151b0da
vperizes <vperizes@gmail.com>
2023-12-08 14:44:26 -0600

    add salt and hashing for password security

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,11 +1,17 @@
 import { StatusCodes } from "http-status-codes";
 import UserModel from "../models/UserModel.js";
+import bcrypt from "bcryptjs";
 
 export const register = async (req, res) => {
   const isFirstAccount = (await UserModel.countDocuments()) === 0;
   req.body.role = isFirstAccount ? "admin" : "user";
+
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(req.body.password, salt);
+  req.body.password = hashedPassword;
+
   const newUser = await UserModel.create(req.body);
-  res.status(StatusCodes.CREATED).json({ newUser });
+  res.status(StatusCodes.CREATED).json({ msg: "user created" });
 };
 
 export const login = async (req, res) => {
