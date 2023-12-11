7bd2b4adc276871a678d9c91dbbf11d9e622f259
vperizes <vperizes@gmail.com>
2023-12-11 12:30:15 -0600

    create JWT logic. Implement in login route.

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,6 +2,7 @@ import { StatusCodes } from "http-status-codes";
 import UserModel from "../models/UserModel.js";
 import { comparePassword, hashPassword } from "../utils/passwordUtils.js";
 import { UnauthenticatedError } from "../errors/customErrors.js";
+import { createJWT } from "../utils/tokenUtils.js";
 
 export const register = async (req, res) => {
   const isFirstAccount = (await UserModel.countDocuments()) === 0;
@@ -22,5 +23,7 @@ export const login = async (req, res) => {
     throw new UnauthenticatedError("invalid credentials");
   }
 
-  res.send("login");
+  const token = createJWT({ userId: user._id, userRole: user.role });
+
+  res.json({ token: token });
 };
