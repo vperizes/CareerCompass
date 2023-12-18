b9e4608c2607ef1326bb3bd7a21f8abc03c74d33
vperizes <vperizes@gmail.com>
2023-12-18 14:10:17 -0600

    set up react toastify for registration page

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "react-toastify/dist/ReactToastify.css";
 import App from "./App.jsx";
 import "./index.css";
 import axios from "axios";
+import { ToastContainer } from "react-toastify";
 
 const data = await axios.get("/api/v1/test");
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
+    <ToastContainer position="top-center" />
   </React.StrictMode>
 );
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -2,6 +2,7 @@ import { Form, redirect, useNavigation, Link } from "react-router-dom";
 import Wrapper from "../assets/wrappers/RegisterAndLoginPage";
 import { Logo, FormInput } from "../components/index";
 import axios from "axios";
+import { toast } from "react-toastify";
 
 export const resgisterAction = async ({ request }) => {
   const formData = await request.formData(); //provides interface
@@ -9,9 +10,10 @@ export const resgisterAction = async ({ request }) => {
   const data = Object.fromEntries(formData); //returns an object from an array of arrays
   try {
     await axios.post("/api/v1/auth/register", data); //make req back to server
+    toast.success("Registration successful");
     return redirect("/login");
   } catch (error) {
-    console.log(error);
+    toast.error(error?.response?.data?.msg);
     return error;
   }
 };
