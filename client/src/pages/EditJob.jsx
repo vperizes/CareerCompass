e7952064539d77fb12515c923405211498474efc
vperizes <vperizes@gmail.com>
2023-12-22 22:37:47 -0600

    implement front end to display single job. add edit job action to patch  request

--- client/src/pages/EditJob.jsx
+++ client/src/pages/EditJob.jsx
@@ -16,24 +16,61 @@ export const editJobLoader = async ({ params }) => {
   }
 };
 
-export const editJobAction = async ({ request }) => {
+export const editJobAction = async ({ request, params }) => {
   const formData = await request.formData();
   const data = Object.fromEntries(formData);
 
   try {
-    await axios.patch("/api/v1/jobs/:id", data);
-    toast.success("Job editted");
+    await axios.patch(`/api/v1/jobs/${params.id}`, data);
+    toast.success("Job edited successfully");
     return redirect("/dashboard/all-jobs");
   } catch (error) {
-    toast.error(error?.request?.data?.msg);
+    toast.error(error?.response?.data?.msg);
     return error;
   }
 };
 
 const EditJob = () => {
   const { job } = useLoaderData();
-  console.log(job);
 
-  return <h1>EditJob</h1>;
+  const navigation = useNavigation();
+  const isSubmitting = navigation.state === "submitting";
+
+  return (
+    <Wrapper>
+      <Form method="post" className="form">
+        <h4 className="form-title">Edit job</h4>
+        <div className="form-center">
+          <FormInput type="text" name="position" defaultValue={job.position} />
+          <FormInput type="text" name="company" defaultValue={job.company} />
+          <FormInput
+            type="text"
+            name="jobLocation"
+            labelText="Job Location"
+            defaultValue={job.jobLocation}
+          />
+          <FormInputSelect
+            name="jobStatus"
+            labelText="job status"
+            defaultValue={job.JOB_STATUS}
+            list={Object.values(JOB_STATUS)}
+          />
+          <FormInputSelect
+            name="jobType"
+            labelText="job type"
+            defaultValue={job.JOB_TYPE}
+            list={Object.values(JOB_TYPE)}
+          />
+          <button
+            type="submit"
+            className="btn btn-block form-btn"
+            disabled={isSubmitting}
+          >
+            {isSubmitting ? "Submitting..." : "submit"}
+          </button>
+        </div>
+      </Form>
+    </Wrapper>
+  );
 };
 export default EditJob;
