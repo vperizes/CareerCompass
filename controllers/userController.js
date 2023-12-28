8816c9632910f1d7b009aa65a57f5dee5d3e867f
vperizes <vperizes@gmail.com>
2023-12-27 23:56:28 -0600

    update user functionality. logic to upload image to cloudinary and delete previous image

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,8 @@
 import UserModel from "../models/UserModel.js";
 import jobModel from "../models/jobModel.js";
-
 import { StatusCodes } from "http-status-codes";
+import cloudinary from "cloudinary";
+import { promises as fs } from "fs";
 
 export const getCurrentUser = async (req, res) => {
   const user = await UserModel.findOne({ _id: req.user.userId });
@@ -17,10 +18,26 @@ export const getAppStats = async (req, res) => {
 };
 
 export const updateUser = async (req, res) => {
-  console.log(req.file);
-  //password value not being added to update controller
-  const obj = { ...req.body };
-  delete obj.password;
-  const updatedUser = await UserModel.findByIdAndUpdate(req.user.userId, obj);
+  const newUserInfo = { ...req.body };
+  delete newUserInfo.password; //password value omitted from update controller
+
+  //update image only if user is sending image
+  if (req.file) {
+    const response = await cloudinary.v2.uploader.upload(req.file.path);
+    await fs.unlink(req.file.path); //removes images locally if image can be uploaded to cloudinary
+    newUserInfo.avatar = response.secure_url;
+    newUserInfo.avatarPublicId = response.public_id;
+  }
+
+  const updatedUser = await UserModel.findByIdAndUpdate(
+    req.user.userId,
+    newUserInfo
+  );
+
+  //removes any old image from cloudinary when user uploads new image
+  if (req.file && updatedUser.avatarPublicId) {
+    await cloudinary.v2.uploader.destroy(updatedUser.avatarPublicId);
+  }
+
   res.status(StatusCodes.OK).json({ msg: "user updated" });
 };
