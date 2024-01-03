cfc0626b5f962deae99610ddb60544ce947e9972
vperizes <vperizes@gmail.com>
2024-01-02 20:00:44 -0600

    add bar chart and area chart so visulizes application data per month

--- client/src/components/BarChart.jsx
+++ client/src/components/BarChart.jsx
@@ -1,4 +1,24 @@
-const BarChart = () => {
-  return <h1>BarChart</h1>;
+import {
+  ResponsiveContainer,
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+} from "recharts";
+
+const BarChartComponent = ({ data }) => {
+  return (
+    <ResponsiveContainer width="100%" height={300}>
+      <BarChart data={data} margin={{ top: 50 }}>
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="date" />
+        <YAxis allowDecimals={false} />
+        <Tooltip contentStyle={{ backgroundColor: "#1e293b" }} cursor={false} />
+        <Bar dataKey="count" stroke="#2cb1bc" fill="#b3f8fd" barSize={100} />
+      </BarChart>
+    </ResponsiveContainer>
+  );
 };
-export default BarChart;
+export default BarChartComponent;
--- client/src/components/ChartsContainer.jsx
+++ client/src/components/ChartsContainer.jsx
@@ -1,4 +1,19 @@
-const ChartsContainer = () => {
-  return <h1>ChartsContainer</h1>;
+import { useState } from "react";
+import Wrapper from "../assets/wrappers/ChartsContainer";
+import BarChart from "./BarChart";
+import AreaChart from "./AreaChart";
+
+const ChartsContainer = ({ data }) => {
+  const [barChart, setBarChart] = useState(true);
+
+  return (
+    <Wrapper>
+      <h4>Monthly Applicaitons</h4>
+      <button type="button" onClick={() => setBarChart(!barChart)}>
+        {barChart ? "Area Chart" : "Bar Chart"}
+      </button>
+      {barChart ? <BarChart data={data} /> : <AreaChart data={data} />}
+    </Wrapper>
+  );
 };
 export default ChartsContainer;
