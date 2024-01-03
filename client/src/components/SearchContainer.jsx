169cfa76373e93cd370f66324bd1fe8f98f95d30
vperizes <vperizes@gmail.com>
2024-01-03 15:41:25 -0600

    search form setup on frontend. add params object to loader get request

--- client/src/components/SearchContainer.jsx
+++ client/src/components/SearchContainer.jsx
@@ -1,4 +1,40 @@
+import { Form, Link } from "react-router-dom";
+import Wrapper from "../assets/wrappers/DashboardFormPage";
+import { FormInput, FormInputSelect, SubmitBtn } from ".";
+import { JOB_SORT_BY, JOB_STATUS, JOB_TYPE } from "../../../utils/constants";
+
 const SearchContainer = () => {
-  return <h2>SearchContainer</h2>;
+  return (
+    <Wrapper>
+      <Form className="form">
+        <h5 className="form-title">search form</h5>
+        <div className="form-center">
+          <FormInput type="search" name="search" />
+          <FormInputSelect
+            name="jobStatus"
+            labelText="job status"
+            list={["all", ...Object.values(JOB_STATUS)]}
+            defaultValue="all"
+          />
+          <FormInputSelect
+            name="jobType"
+            labelText="job type"
+            list={["all", ...Object.values(JOB_TYPE)]}
+            defaultValue="all"
+          />
+          <FormInputSelect
+            name="sort"
+            list={Object.values(JOB_SORT_BY)}
+            defaultValue="newest"
+          />
+          <Link to="/dashboard/all-jobs" className="btn form-btn delete-btn">
+            Reset search values
+          </Link>
+          {/* temp btn */}
+          <SubmitBtn formBtn />
+        </div>
+      </Form>
+    </Wrapper>
+  );
 };
 export default SearchContainer;
--- client/src/pages/AllJobs.jsx
+++ client/src/pages/AllJobs.jsx
@@ -5,8 +5,17 @@ import { JobsContainer, SearchContainer } from "../components";
 import { createContext, useContext } from "react";
 
 export const allJobsLoader = async ({ request }) => {
+  console.log(request.url);
+
+  //make params into object
+  const params = Object.fromEntries([
+    ...new URL(request.url).searchParams.entries(),
+  ]);
+  console.log(params);
   try {
-    const { data } = await axios.get("/api/v1/jobs");
+    const { data } = await axios.get("/api/v1/jobs", {
+      params: params,
+    });
     return { data };
   } catch (error) {
     toast.error(error?.response?.data?.msg);
