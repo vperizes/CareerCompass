e48afc21df435a7f14076ab51d0534daf8ffb483
vperizes <vperizes@gmail.com>
2024-01-10 23:29:53 -0600

    set up react query in stats loader

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -85,7 +85,7 @@ const router = createBrowserRouter([
           {
             path: "stats",
             element: <Stats />,
-            loader: statsLoader,
+            loader: statsLoader(queryClient),
             errorElement: <ErrorElement />,
           },
           {
--- client/src/pages/Stats.jsx
+++ client/src/pages/Stats.jsx
@@ -3,23 +3,23 @@ import { useLoaderData } from "react-router-dom";
 import { ChartsContainer, StatsContainer } from "../components";
 import { useQuery } from "@tanstack/react-query";
 
-export const statsLoader = async () => {
-  return null;
-  const response = await axios.get("/api/v1/jobs/stats");
-  return response.data;
+const statsQuery = {
+  queryKey: ["stats"],
+  queryFn: async () => {
+    const response = await axios.get("/api/v1/jobs/stats");
+    return response.data; //getting axios data from return query data (this replaces the need for data.data in useQuery)
+  },
+};
+
+//can only use hooks in component or custom hooks -> need to pass queryClient (App.jsx) directly to loader -> achieve this by making statsLoader a function that returns a function
+export const statsLoader = (queryClient) => async () => {
+  const data = await queryClient.ensureQueryData(statsQuery); //ensureQueryData -> async function that gets an existing query's cached data.
+  return data;
 };
 
 const Stats = () => {
-  //const { defaultStats, monthlyApplications } = useLoaderData();
-  const { isLoading, isError, data } = useQuery({
-    queryKey: ["stats"],
-    queryFn: () => axios.get("/api/v1/jobs/stats"),
-  });
-
-  if (isLoading) return <h4>Loading...</h4>;
-  if (isLoading) return <h4>Error...</h4>;
-
-  const { defaultStats, monthlyApplications } = data.data;
+  const { data } = useQuery(statsQuery); //if cached data existed, useQuery will get it. if not, then ensureQueryData will refetch on autofocus.
+  const { defaultStats, monthlyApplications } = data;
 
   return (
     <>
