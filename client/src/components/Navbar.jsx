796e65edc97b71958009c306f4fe0abea4013df2
vperizes <vperizes@gmail.com>
2024-04-04 16:39:08 -0500

    add navlink to logo

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -3,6 +3,7 @@ import { FaAlignLeft } from "react-icons/fa";
 import Logo from "./Logo";
 import { useDashboardContext } from "../pages/DashboardLayout";
 import ProfileContainer from "./ProfileContainer";
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
   const { toggleSidebar } = useDashboardContext();
@@ -12,7 +13,10 @@ const Navbar = () => {
         <button type="button" className="toggle-btn" onClick={toggleSidebar}>
           <FaAlignLeft />
         </button>
-        <Logo className="logo" />
+        <NavLink to="." key="add-job">
+          <Logo className="logo" />
+        </NavLink>
+
         <h4 className="logo-text">Dashboard</h4>
         <div className="btn-container">
           <ProfileContainer />
