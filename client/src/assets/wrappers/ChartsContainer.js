15c32907d52c60dfa774a8066190ad0a1453d733
vperizes <vperizes@gmail.com>
2024-04-17 23:21:38 -0500

    remove bar chart. modify area chart display

--- client/src/assets/wrappers/ChartsContainer.js
+++ client/src/assets/wrappers/ChartsContainer.js
@@ -1,16 +1,10 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 const Wrapper = styled.section`
   margin-top: 4rem;
   text-align: center;
-  button {
-    background: transparent;
-    border-color: transparent;
-    text-transform: capitalize;
-    color: var(--primary-500);
-    font-size: 1.25rem;
-    cursor: pointer;
-  }
+  display: grid;
+
   h4 {
     text-align: center;
     margin-bottom: 0.75rem;
